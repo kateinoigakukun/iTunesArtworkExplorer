ef91407a3828b2df20cff344e27a301342149f5b
Saito Yuta <kateinoigakukun@gmail.com>
2016-08-06 09:35:40 +0900

    fix spell

---
+++ iTunesArtworkExplorer/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿using System.Windows.Forms;
+using System.Collections;
+using System.Text;
+using System.Drawing;
+using System.ComponentModel;
+using System.Data;
+
+namespace iTunesArtworkExplorer
+{
+    partial class iTunesArtworkExplorer
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(iTunesArtworkExplorer));
+            this.artworkPictureBox = new System.Windows.Forms.PictureBox();
+            this.trackNameLabel = new System.Windows.Forms.Label();
+            this.trackArtistLabel = new System.Windows.Forms.Label();
+            this.apiResultGridView = new System.Windows.Forms.DataGridView();
+            this.dt = new System.Data.DataTable();
+            this.artworkDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artistDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.artworkPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.apiResultGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // artworkPictureBox
+            // 
+            this.artworkPictureBox.Location = new System.Drawing.Point(13, 13);
+            this.artworkPictureBox.Name = "artworkPictureBox";
+            this.artworkPictureBox.Size = new System.Drawing.Size(151, 154);
+            this.artworkPictureBox.TabIndex = 0;
+            this.artworkPictureBox.TabStop = false;
+            // 
+            // trackNameLabel
+            // 
+            this.trackNameLabel.AutoSize = true;
+            this.trackNameLabel.Location = new System.Drawing.Point(171, 13);
+            this.trackNameLabel.Name = "trackNameLabel";
+            this.trackNameLabel.Size = new System.Drawing.Size(35, 12);
+            this.trackNameLabel.TabIndex = 1;
+            this.trackNameLabel.Text = "";
+            this.trackNameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // trackArtistLabel
+            // 
+            this.trackArtistLabel.AutoSize = true;
+            this.trackArtistLabel.Location = new System.Drawing.Point(170, 25);
+            this.trackArtistLabel.Name = "trackArtistLabel";
+            this.trackArtistLabel.Size = new System.Drawing.Size(35, 12);
+            this.trackArtistLabel.TabIndex = 2;
+            this.trackArtistLabel.Text = "";
+            // 
+            // apiResultGridView
+            // 
+            this.apiResultGridView.AllowUserToAddRows = false;
+            this.apiResultGridView.AllowUserToDeleteRows = false;
+            this.apiResultGridView.AutoGenerateColumns = false;
+            this.apiResultGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.apiResultGridView.DataSource = this.dt;
+            this.apiResultGridView.Location = new System.Drawing.Point(13, 174);
+            this.apiResultGridView.MultiSelect = false;
+            this.apiResultGridView.Name = "apiResultGridView";
+            this.apiResultGridView.ReadOnly = true;
+            this.apiResultGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.apiResultGridView.RowTemplate.Height = 21;
+            this.apiResultGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.apiResultGridView.Size = new System.Drawing.Size(1000, 500);
+            this.apiResultGridView.TabIndex = 0;
+            this.apiResultGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // artworkDataGridViewTextBoxColumn
+            // 
+            this.artworkDataGridViewTextBoxColumn.DataPropertyName = "Artwork";
+            this.artworkDataGridViewTextBoxColumn.HeaderText = "Artwork";
+            this.artworkDataGridViewTextBoxColumn.Name = "artworkDataGridViewTextBoxColumn";
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "Title";
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            // 
+            // artistDataGridViewTextBoxColumn
+            // 
+            this.artistDataGridViewTextBoxColumn.DataPropertyName = "Artist";
+            this.artistDataGridViewTextBoxColumn.HeaderText = "Artist";
+            this.artistDataGridViewTextBoxColumn.Name = "artistDataGridViewTextBoxColumn";
+            //
+            // idDataGridViewTextBoxColumn
+            //
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "trackID";
+            this.idDataGridViewTextBoxColumn.HeaderText = "trackID";
+            this.idDataGridViewTextBoxColumn.Name = "trackID";
+            // 
+            // iTunesArtworkExplore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 711);
+            this.Controls.Add(this.apiResultGridView);
+            this.Controls.Add(this.trackArtistLabel);
+            this.Controls.Add(this.trackNameLabel);
+            this.Controls.Add(this.artworkPictureBox);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "iTunesArtworkExplorer";
+            this.Text = "iTunesArtworkExplorer";
+            ((System.ComponentModel.ISupportInitialize)(this.artworkPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.apiResultGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox artworkPictureBox;
+        private System.Windows.Forms.Label trackNameLabel;
+        private System.Windows.Forms.Label trackArtistLabel;
+        private System.Windows.Forms.DataGridView apiResultGridView;
+        private DataGridViewImageColumn artworkDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn artistDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private DataTable dt;
+    }
+}
+
