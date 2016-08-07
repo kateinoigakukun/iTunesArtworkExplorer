b87fe427d004e5f9ae1ce55a328923029033b678
Saito Yuta <kateinoigakukun@gmail.com>
2016-08-08 08:12:44 +0900

    MacOS X10.9

---
+++ iTunesArtworkExplorer for Mac/iTunesArtworkExplorer for Mac/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  iTunesArtworkExplorer for Mac
+//
+//  Created by SaitoYuta on 2016/08/08.
+//  Copyright © 2016年 SaitoYuta. All rights reserved.
+//
+
+import Cocoa
+
+class test: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
